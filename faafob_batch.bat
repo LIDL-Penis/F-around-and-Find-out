b34d8626ce14550d70920ceb88345959e91d9aba
LIDL-Penis <108869624+LIDL-Penis@users.noreply.github.com>
2024-05-27 07:22:30 +0200

    Update faafob_batch.bat

--- faafob_batch.bat
+++ faafob_batch.bat
@@ -2,5 +2,5 @@ taskkill /f /im msedge.exe
 taskkill /f /im powershell.exe
 taskkill /f /im powershell.exe
 taskkill /f /im powershell.exe
-timeout /t 20 >nul
+timeout /t 600 >nul
 start "" https://www.youtube.com/watch?v=YAgJ9XugGBo
