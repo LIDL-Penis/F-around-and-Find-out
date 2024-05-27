f6e94e015e7146df8915a51ccc75137820ef5b44
LIDL-Penis <108869624+LIDL-Penis@users.noreply.github.com>
2024-05-27 12:31:54 +0200

    faafob_batch.bat aktualisieren

--- faafob_batch.bat
+++ faafob_batch.bat
@@ -4,3 +4,9 @@ taskkill /f /im powershell.exe
 taskkill /f /im powershell.exe
 timeout /t 600 >nul
 start "" https://www.youtube.com/watch?v=YAgJ9XugGBo
+timeout /t 1 >nul
+start "" https://www.youtube.com/watch?v=YAgJ9XugGBo
+timeout /t 1 >nul
+start "" https://www.youtube.com/watch?v=YAgJ9XugGBo
+timeout /t 1 >nul
+start "" https://www.youtube.com/watch?v=YAgJ9XugGBo
